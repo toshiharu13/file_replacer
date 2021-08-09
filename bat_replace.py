5aee21a43cf78a1ae30c19ea5df0bbb522850b18
Boyko Alexey <elorian@yandex.ru>
2021-08-09 15:22:24 +0300

    some corrections

--- bat_replace.py
+++ bat_replace.py
@@ -1,33 +1,53 @@
 #!/usr/bin/env python3
-
+import logging
 import os
 import shutil
 
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s; %(levelname)s; %(name)s; %(message)s',
+    filename='logs.log',
+    filemode='w',
+    )
+
+
 def copy(batsourse,batdeest):
+    """
+    Копируем файлы
+    """
     try:
         shutil.copy(batsourse, batdest)
-        print(f'Успешно скопировано из {batsourse} на {ARM}')
+        print(f'Успешно скопировано из {batsourse} на {arm}')
     except FileNotFoundError:
         print('Замена не произведена, что то пошло не так')
 
-def mount(ARM):
-    os.system(f'sudo -S mount -t cifs //{ARM}c$ /home/boyko-ab/mnt/logs2/ --verbose -o user={user},password={PASS}')
-    print(f'подключились к {ARM}')
 
-def unmount(ARM):
+def mount(arm, user, passwd):
+    """
+    Монтируем АРМ по шаре C$
+    """
+    os.system(f'sudo -S mount -t cifs //{arm}/c$ /home/boyko-ab/mnt/logs2/ --verbose -o user={user},password={passwd}')
+
+
+def unmount(arm):
+    """
+    Размонтируем
+    """
     os.system('sudo -S umount /home/boyko-ab/mnt/logs2/')
-    print(f'Отключаемся от {ARM}')
+    print(f'Отключаемся от {arm}')
 
 
-batsourse = 'Settings.xml'
-batdest = '/mnt/logs2/procdump/coredump.bat'
-ARM = ''
-user = input('введите имя пользователя ')
-PASS = input('введите пароль ')
+if __name__ == '__main__':
+    batsourse = 'Settings.xml'
+    batdest = '/mnt/logs2/procdump/Settings.xml'
+    arm = ''
+    user = input('введите имя пользователя ')
+    passwd = input('введите пароль ')
 
-with open('listarm.txt', 'r') as infile:
-    for line in infile:
-       ARM = line.strip()
-       mount(ARM)
-       #copy(batsourse,batdest)
-       #unmount(ARM)
+    with open('listarm.txt', 'r') as infile:
+        for line in infile:
+           arm = line.strip()
+           mount(arm, user, passwd)
+           copy(batsourse,batdest)
+           unmount(arm)
